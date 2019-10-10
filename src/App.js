1064221f0ca5a18a3318c3919ee33e7271f81320
Tim Hütz <tim@huetz.biz>
2019-10-10 14:50:24 +0200

    chore: add simple capturing

--- src/App.js
+++ src/App.js
@@ -1,26 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Camera from "react-html5-camera-photo";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentWillMount() {
+    this.setState({
+      dataUri: ""
+    });
+  }
+
+  onTakePhoto(dataUri) {
+    this.setState({
+      dataUri
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Camera
+          onTakePhoto={dataUri => {
+            this.onTakePhoto(dataUri);
+          }}
+        />
+        <div>{this.state.dataUri}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
